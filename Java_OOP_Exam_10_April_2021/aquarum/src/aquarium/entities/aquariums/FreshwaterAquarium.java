ce6c61d205775fab5ce39480ac48548cc87241e4
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2022-01-03 13:21:42 +0200

    Delete Java_OOP_Exam_10_April_2021 directory

--- Java_OOP_Exam_10_April_2021/aquarum/src/aquarium/entities/aquariums/FreshwaterAquarium.java
+++
@@ -1,8 +0,0 @@
-package aquarium.entities.aquariums;
-
-public class FreshwaterAquarium extends BaseAquarium{
-    private static final int capacity = 50;
-    public FreshwaterAquarium(String name) {
-        super(name, capacity);
-    }
-}
--- Java_OOP_Exam_10_April_2021/aquarum/src/aquarium/entities/decorations/Ornament.java
+++
@@ -1,9 +0,0 @@
-package aquarium.entities.decorations;
-
-public class Ornament extends BaseDecoration{
-    private static final int comfort = 1;
-    private static final double price = 5;
-    public Ornament() {
-        super(comfort, price);
-    }
-}
